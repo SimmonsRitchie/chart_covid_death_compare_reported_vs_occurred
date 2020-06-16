8507db2c862a0edc860848ab31225d900aef8663
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-06-16 14:04:25 -0400

    Fix test

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,7 +29,7 @@ class Main extends React.Component {
           <Body data={this.props.data} /> 
           <Footer 
             byline="Daniel Simmons-Ritchie"
-            source="Reported death data is based on statistics reported on the website of the Pa. Departhment of Health and collected daily by Spotlight PA/the Philadelphia Inquirer between March to June, 2020. Occurred death data is based on separate Pa. Department of Health statistics collated on June 16, 2020."
+            source="Reported death data is based on statistics reported on the website of the Pa. Department of Health and collected daily by Spotlight PA/the Philadelphia Inquirer between March to June, 2020. Occurred death data is based on Pa. Department of Health statistics collated on June 16, 2020."
             outlet="Spotlight PA"
             outletUrl="https://www.spotlightpa.org/"
           />

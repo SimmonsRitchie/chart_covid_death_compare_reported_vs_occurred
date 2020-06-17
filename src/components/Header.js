ac5628daababbce8692db89b791e7f16a8978190
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-06-17 14:27:36 -0400

    Fix typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
       <div className="header__container-inner">
         <div>
           <h2 className="title is-4 has-text-weight-bold">Pennsylvania's erratic death data</h2>
-          <h2 className="subtitle is-6">Pennsylvania has repeatedly changed how it counts coronavirus deaths since March and has often been late in reporting new deaths. That's led to unusual spikes in pubicly-available data that has, at times, made it difficult to understand how the state's death rate is trending.
+          <h2 className="subtitle is-6">Pennsylvania has repeatedly changed how it counts coronavirus deaths since March and has often been late in reporting new deaths. That’s led to unusual spikes in publicly available data that has, at times, made it difficult to understand how the state’s death rate is trending.
           </h2>
         </div>
       </div>
